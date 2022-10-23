76d9c441c97cea7fa3553aa61a6688d6358dd633
xmengweb <dd2419247021@163.com>
2022-10-23 13:49:46 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="app">
-    <h2>hello</h2>
-    <router-link to="/login">登录</router-link>
-    <router-link to="/main">首页</router-link>
     <router-view></router-view>
-    <el-button type="primary">Primary</el-button>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
